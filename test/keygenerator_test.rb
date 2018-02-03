701e03e2a1dd60807ec539d9892a71b05a09caa3
hmesander <haleymesander@gmail.com>
2018-02-03 13:00:44 -0700

    Refactor of key generator tests.

--- test/keygenerator_test.rb
+++ test/keygenerator_test.rb
@@ -1,5 +1,4 @@
-require './lib/keygenerator'
-require 'minitest'
+require_relative 'keygenerator'
 require 'minitest/autorun'
 require 'minitest/pride'
 
@@ -10,7 +9,7 @@ class KeyGeneratorTest < Minitest::Test
     assert_instance_of KeyGenerator, key_generator
   end
 
-  def test_that_key_generated_is_an_integer
+  def test_that_key_generated_is_a_string
     key_generator = KeyGenerator.new
 
     assert_instance_of String, key_generator.new_key
@@ -24,33 +23,33 @@ class KeyGeneratorTest < Minitest::Test
 
   def test_for_first_letter_rotation_method
     key_generator = KeyGenerator.new
-    key = key_generator.key
-    concatenated_key = key[0] + key[1]
+    key_generator.new_key
+    concatenated_key = (key_generator.key[0] + key_generator.key[1]).to_i
 
-    assert_equal concatenated_key.to_i, key_generator.first_letter_rotation
+    assert_equal concatenated_key, key_generator.first_letter_rotation
   end
 
   def test_for_second_letter_rotation_method
     key_generator = KeyGenerator.new
-    key = key_generator.key
-    concatenated_key = key[1] + key[2]
+    key_generator.new_key
+    concatenated_key = (key_generator.key[1] + key_generator.key[2]).to_i
 
-    assert_equal concatenated_key.to_i, key_generator.second_letter_rotation
+    assert_equal concatenated_key, key_generator.second_letter_rotation
   end
 
   def test_for_third_letter_rotation_method
     key_generator = KeyGenerator.new
-    key = key_generator.key
-    concatenated_key = key[2] + key[3]
+    key_generator.new_key
+    concatenated_key = (key_generator.key[2] + key_generator.key[3]).to_i
 
-    assert_equal concatenated_key.to_i, key_generator.third_letter_rotation
+    assert_equal concatenated_key, key_generator.third_letter_rotation
   end
 
   def test_for_fourth_letter_rotation_method
     key_generator = KeyGenerator.new
-    key = key_generator.key
-    concatenated_key = key[3] + key[4]
+    key_generator.new_key
+    concatenated_key = (key_generator.key[3] + key_generator.key[4]).to_i
 
-    assert_equal concatenated_key.to_i, key_generator.fourth_letter_rotation
+    assert_equal concatenated_key, key_generator.fourth_letter_rotation
   end
 end
