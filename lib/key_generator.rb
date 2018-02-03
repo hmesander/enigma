0c02a09adfe5680ff2fe481613ff5e66643a03e6
gijolopez <glopez22@me.com>
2018-02-03 15:46:28 -0700

    added KeyGenerator class

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,34 +1,4 @@
 class KeyGenerator
 
-  def initialize
-    @a = []
-    @b = []
-    @c = []
-    @d = []
-  end
-
-
-  def key
-    numbers = [*0..9]
-    numbers.each do |num|
-       # <<num.sample(5)[0..1]
-  end
-    # numbers.sample(5)[0..1]
-    # a = numbers.sample(5)
-        # # b = numbers.sample(5)
-  end
-
-
-  end
-
-  def group_Key
-    a = []
-    b = []
-    c = []
-    d = []
-  key.index[]
 
 end
-
-key_generator = KeyGenerator.new
-p key_generator.key
