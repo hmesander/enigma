e26f2a517e12f50f08c5a1bdb20221ef6273cb67
gijolopez <glopez22@me.com>
2018-02-04 15:34:40 -0700

    committed to switch branches

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,13 +1,17 @@
 require 'Date'
 class Offset
-  attr_reader :a, :b, :c, :d
+  attr_reader :first_a_rotation,
+              :second_b_rotation,
+              :third_c_rotation,
+              :fourth_d_rotation
+  # :a, :b, :c, :d
 
-  def initialize
-    @a = []
-    @b = []
-    @c = []
-    @d = []
-  end
+  # def initialize
+  #   @a = []
+  #   @b = []
+  #   @c = []
+  #   @d = []
+  # end
 
   def date
     current_date = Date.today.strftime('%d%m%y')
@@ -18,10 +22,26 @@ class Offset
      (date**2).to_s
   end
 
-  def last_four_digits
-    @a << squared_date[-4].to_i
-    @b << squared_date[-3].to_i
-    @c << squared_date[-2].to_i
-    @d << squared_date[-1].to_i
+  # def last_four_digits
+  #   @a << squared_date[-4].to_i
+  #   @b << squared_date[-3].to_i
+  #   @c << squared_date[-2].to_i
+  #   @d << squared_date[-1].to_i
+  # end
+
+  def first_a_rotation
+    squared_date[-4].to_i
+  end
+
+  def second_b_rotation
+    squared_date[-3].to_i
+  end
+
+  def third_c_rotation
+    squared_date[-2].to_i
+  end
+
+  def fourth_d_rotation
+    squared_date[-1].to_i
   end
 end
