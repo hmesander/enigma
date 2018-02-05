0bb3768934bf588ae72b10a2fd40310ed244ca05
hmesander <haleymesander@gmail.com>
2018-02-04 19:51:30 -0700

    Refactor of offset class to calculate total rotation values

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -2,36 +2,53 @@ require 'minitest/pride'
 require 'minitest/autorun'
 require './lib/offset'
 
+# Tests for rotation offset calculator.
 class OffsetTest < Minitest::Test
-
   def test_if_exists
     offset = Offset.new
 
     assert_instance_of Offset, offset
   end
 
-  def test_date_is_formatted_correctly
-    offset = Offset.new
-
-    assert_equal 40218, offset.date
-  end
-
-  def test_current_date_squared
+  def test_date_squared
     offset = Offset.new
     offset.date
 
-    assert_equal "1617487524", offset.squared_date
+    assert_equal '1617487524', offset.squared_date
   end
 
   def test_last_four_digits
     offset = Offset.new
-    offset.date
     offset.squared_date
-    offset.last_four_digits
 
-    assert_equal [7], offset.a
-    assert_equal [5], offset.b
-    assert_equal [2], offset.c
-    assert_equal [4], offset.d
+    assert_equal '7524', offset.last_four_digits
+  end
+
+  def test_a_total_rotation
+    offset = Offset.new
+    offset.squared_date
+
+    assert_equal 3, offset.a_rotation
+  end
+
+  def test_b_total_rotation
+    offset = Offset.new
+    offset.squared_date
+
+    assert_equal 4, offset.b_rotation
+  end
+
+  def test_c_total_rotation
+    offset = Offset.new
+    offset.squared_date
+
+    assert_equal 5, offset.c_rotation
+  end
+
+  def test_d_total_rotation
+    offset = Offset.new
+    offset.squared_date
+
+    assert_equal 6, offset.d_rotation
   end
 end
